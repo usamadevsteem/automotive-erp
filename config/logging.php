84ac8a53884d7036f0981588ddb08f935e9f0a1c
Usama Arshad <203601813+usamadevsteem@users.noreply.github.com>
2026-09-02 16:31:41 +0500

    Restore Laravel logging configuration

---
+++ config/logging.php
@@ -0,0 +1,20 @@
+<?php
+
+use Monolog\Handler\NullHandler;
+use Monolog\Handler\StreamHandler;
+use Monolog\Processor\PsrLogMessageProcessor;
+
+return [
+    'default' => env('LOG_CHANNEL', 'stderr'),
+    'deprecations' => ['channel' => env('LOG_DEPRECATIONS_CHANNEL', 'null'), 'trace' => false],
+    'channels' => [
+        'stack' => ['driver' => 'stack', 'channels' => explode(',', (string) env('LOG_STACK', 'stderr')), 'ignore_exceptions' => false],
+        'single' => ['driver' => 'single', 'path' => storage_path('logs/laravel.log'), 'level' => env('LOG_LEVEL', 'debug'), 'replace_placeholders' => true],
+        'stderr' => [
+            'driver' => 'monolog', 'level' => env('LOG_LEVEL', 'debug'), 'handler' => StreamHandler::class,
+            'handler_with' => ['stream' => 'php://stderr'], 'processors' => [PsrLogMessageProcessor::class],
+        ],
+        'null' => ['driver' => 'monolog', 'handler' => NullHandler::class],
+        'emergency' => ['path' => storage_path('logs/laravel.log')],
+    ],
+];
